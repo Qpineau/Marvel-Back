ccc9d0ce96137d3c6da3548fa911bbff5742d5a6
Quentin Pineau <quentin.pineau@gmail.com>
2021-01-25 18:03:54 +0100

    char modified

--- routes/marvel.js
+++ routes/marvel.js
@@ -51,7 +51,7 @@ router.get("/comics/:characterId", async (req, res) => {
     const hash = md5(ts + privateKey + publicKey);
 
     const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/comics/${characterId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+      `http://gateway.marvel.com/v1/public/characters/${characterId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`
     );
     res.json(response.data);
   } catch (error) {
